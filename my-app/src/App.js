5d5033544635f2e76c16912b9d6c4e08b4ee2a1b
Kevin323c <108781095+Aimless-Wanderer@users.noreply.github.com>
2023-03-08 01:38:54 -0800

    s

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages';
+import Inventory from './pages/Inventory';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/Inventory" component={Inventory} />
+      </Switch>
     </Router>
   );
 }
---
+++ my-app/src/pages/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+  return (
+    <div>
+      <h1>Inventory Page</h1>
+      <p>This is the Inventory page.</p>
+    </div>
+  );
+}
+
+export default Inventory;
