8753247ad844933392278f4d44dbaac7a33a7abf
Kevin323c <108781095+Aimless-Wanderer@users.noreply.github.com>
2023-03-08 02:01:24 -0800

    asdgdasg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import Home from './pages';
-import Inventory from './pages/Inventory';
 
 function App() {
   return (
     <Router>
-      <Route>
-        <Route path="/" component={Home} />
-        <Route path='/Inventory' component = {<Inventory/>} />
-      </Route>
+      <Home />
     </Router>
   );
 }
